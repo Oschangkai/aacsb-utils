25c53066a9f70b11624d3e16cc1d190de0e12c4a
Oschangkai <oschangkai@kaiyeee.tw>
2023-10-16 02:13:32 +0800

    Update: teacher research data

--- tools/export_research.sql
+++ tools/export_research.sql
@@ -1,10 +1,19 @@
 SELECT
-  T.Name AS TeacherName, Type, Research.Title,
+  T.Name AS TeacherName, Research.Title,
+  AddressAuthors, OtherAuthors, OrderAuthors,
+  Seminar, City, Country, Issue,
+  JournalsClass, JournalsName, JournalsStatus, JournalsType,
+  PageStart, PageEnd, Project, Publication, Volume,
+  Year, YearEnd, YearStart, Month, MonthEnd, MonthStart, DayEnd, DayStart, FullText,
+  STRING_AGG(RT.Code, ',') AS ResearchTypeCode
+FROM [ReportGenerator].[Research]
+  LEFT JOIN [ReportGenerator].[Teachers] T on T.Id = Research.TeacherId
+  LEFT JOIN [ReportGenerator].[ResearchResearchType] RRT on Research.Id = RRT.ResearchId
+  LEFT JOIN [ReportGenerator].[ResearchType] RT on RRT.ResearchTypeId = RT.Id
+GROUP BY T.Name, Research.Title,
   AddressAuthors, OtherAuthors, OrderAuthors,
   Seminar, City, Country, Issue,
   JournalsClass, JournalsName, JournalsStatus, JournalsType,
   PageStart, PageEnd, Project, Publication, Volume,
   Year, YearEnd, YearStart, Month, MonthEnd, MonthStart, DayEnd, DayStart, FullText
-FROM [ReportGenerator].[Research]
-  LEFT JOIN [ReportGenerator].[Teachers] T on T.Id = Research.TeacherId
-ORDER BY T.DepartmentId ,Type, Year DESC, YearStart DESC, Month DESC, MonthStart DESC
+ORDER BY COALESCE(Year, YearEnd, YearStart) DESC, COALESCE(Month, MonthEnd, MonthStart) DESC
