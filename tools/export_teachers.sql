526dc772be32c22d0853790e09e8fdce2afa7d0c
Oschangkai <oschangkai@kaiyeee.tw>
2023-11-10 17:46:48 +0800

    Feat: add link informantion

--- tools/export_teachers.sql
+++ tools/export_teachers.sql
@@ -1,4 +1,4 @@
-SELECT t.Name, t.WorkTypeAbbr, t.EnglishName, t.Degree, t.DegreeYear, d.Abbreviation AS Department, Email, EnglishNameInNtustCourse, NameInNtustCourse, q.Abbreviation AS Qualification, ResignDate, Title, Supervisor
+SELECT t.Name, t.WorkTypeAbbr, t.EnglishName, t.Degree, t.DegreeYear, d.Abbreviation AS Department, Email, EnglishNameInNtustCourse, NameInNtustCourse, q.Abbreviation AS Qualification, ResignDate, Title, Supervisor, LinkTo
 FROM [ReportGenerator].Teachers t
   LEFT JOIN [ReportGenerator].[Departments] d on t.DepartmentId = d.Id
   LEFT JOIN [ReportGenerator].[Qualifications] q on q.Id = t.QualificationId
